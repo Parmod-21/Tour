7167e9be66efb2971a79adfa547afe0d5ce45c4d
Parmod-21 <vihugupta135@gmail.com>
2024-11-16 19:22:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react';
+import './App.css';
+import Tours from './Components/Tours';
+ import data from './data';
+
+const App = ()=>{
+
+const [tours, setTours] = useState(data);
+function removeTour(id){
+ const newTours= tours.filter(tour=> tour.id !== id)
+ setTours(newTours)
 }
 
+if(tours.length=== 0){
+  return(
+     <div className='refresh'>
+         <h2>No Turns Left</h2>
+         <button className="btn-white" onClick={()=>setTours(data)}>
+             Refresh
+      </button>
+     </div>
+    
+  )
+}
+
+return(
+  <div className='App'>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+  </div>
+)
+
+}
 export default App;
